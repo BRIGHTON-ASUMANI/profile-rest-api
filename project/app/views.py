d918cbe5b3a299fbe2c43399bb9c816a4ba37bf8
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2019-04-26 17:53:57 +0300

    first api creation

--- project/app/views.py
+++ project/app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+class HelloApiView(APIView):
+    """
+    Test API View
+    """
+    def get(self, request, format=None):
+        """
+        Returns a list of APIView features
+        """
+        an_apiview = [
+            "brighton",
+            "asumani",
+            "angaya"
+        ]
+
+        return Response({'message': 'hello', 'an_apiview': an_apiview})
