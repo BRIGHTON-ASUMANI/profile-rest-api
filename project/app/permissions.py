22fffd174f6ec214d35a3b613e0466ba2fa29b6c
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2019-04-30 16:10:33 +0300

    added earch functionality

---
+++ project/app/permissions.py
@@ -0,0 +1,14 @@
+from rest_framework import permissions
+
+class UpdateOwnProfile(permissions.BasePermission):
+    """
+    Allows users to edit their own profile
+    """
+    def has_object_permissions(self, request, view, obj):
+        """
+        check user is trying to edit their own profile
+        """
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.id == request.user.id
--- project/app/views.py
+++ project/app/views.py
@@ -4,9 +4,13 @@ from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from . import serializers
 from . import models
+from . import permissions
+
 
 # Create your views here.
 
@@ -122,3 +126,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     """
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
