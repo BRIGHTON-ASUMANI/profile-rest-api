c29c71b8e0b8a19519f6c9588854fbc7cf7826bf
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2019-04-30 14:44:57 +0300

    added viewsets

--- project/app/views.py
+++ project/app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -56,3 +57,20 @@ class HelloApiView(APIView):
         Deletes an object
         """
         return Response({'method': 'delete'})
+
+class HelloViewSet(viewsets.ViewSet):
+    """
+    Test API Viewsets
+    """
+    def list(self, request):
+        """
+        Return a hello message
+        """
+
+        a_viewset = [
+            "brighton",
+            "asumani",
+            "angaya"
+        ]
+        return Response({'message': 'hello', 'a_viewset': a_viewset})
+
