864ae1eb4ced8fb60a7cfda640a45c9bcde13215
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2019-04-30 15:06:30 +0300

    added CRUD viewsets

--- project/app/views.py
+++ project/app/views.py
@@ -62,6 +62,7 @@ class HelloViewSet(viewsets.ViewSet):
     """
     Test API Viewsets
     """
+    serializer_class = serializers.HelloSerializer
     def list(self, request):
         """
         Return a hello message
@@ -74,3 +75,41 @@ class HelloViewSet(viewsets.ViewSet):
         ]
         return Response({'message': 'hello', 'a_viewset': a_viewset})
 
+    def create(self, request):
+        """
+        Create a new hello message
+        """
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+
+            return Response({'message':message})
+
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        """
+        Handles getting an object by ID
+        """
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """
+        Handles updating an object
+        """
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """
+        Handles updating part of an object
+        """
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """
+        Handles removing an object
+        """
+        return Response({'http_method': 'DELETE'})
