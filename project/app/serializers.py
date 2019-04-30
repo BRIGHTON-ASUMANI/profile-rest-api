428146265eee6b7c068f0a2dbe1a6780008fcc4e
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2019-04-30 15:43:38 +0300

    profile listing

--- project/app/serializers.py
+++ project/app/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from . import models
 
 class HelloSerializer(serializers.Serializer):
     """
@@ -6,3 +7,26 @@ class HelloSerializer(serializers.Serializer):
     """
 
     name = serializers.CharField(max_length=10)
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """
+    A serializer for our user profile objects
+    """
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'password')
+        extra_kwargs = {'password':{'write_only':True}}
+
+    def create(self, validated_data):
+        """
+        Create and return a new user
+        """
+        user = models.UserProfile(
+            email = validated_data['email'],
+            name = validated_data['name']
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
--- project/app/urls.py
+++ project/app/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 router = DefaultRouter()
 
-router.register('hello viewset', views.HelloViewSet, base_name='hello-viewset')
-
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+router.register('profile', views.UserProfileViewSet)
 urlpatterns = [
     url(r'^hello-view/', views.HelloApiView.as_view()),
     url(r'', include(router.urls))
